b18236e02093d0e38066a8120afea84f84cad9a7
chengzi <ttghost@126.com>
2020-07-09 17:12:38 +0800

    fix: 更新vue依赖

--- src/constant/index.js
+++ src/constant/index.js
@@ -1,10 +1,6 @@
-import { modes, IVIEW_V2, IVIEW_V3 } from './static'
-import { map2to3 } from './icon-type-map'
+import { modes } from './static'
 
 export { default as defaultSchema } from './default-schema'
 export {
-  modes,
-  IVIEW_V2,
-  IVIEW_V3,
-  map2to3
+  modes
 }
