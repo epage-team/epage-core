c00b2c2f7284cdd1aba9069cf25e5a95df1efcf9
chengzi <ttghost@126.com>
2021-02-18 17:33:08 +0800

    gix: 更新ReactRender

--- src/render/ReactRender.js
+++ src/render/ReactRender.js
@@ -1,14 +1,3 @@
-// import Vue from 'vue'
-// import Vuex from 'vuex'
-// import Rule from '../rule'
-// import Store from '../store'
-// import {
-//   isArray,
-//   isFunction,
-//   isPlainObject,
-//   usePlugins
-// } from '../helper'
-
 export default class Render {
   constructor (option) {
     
